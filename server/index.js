52326c81c1d0a410af7aadb19c0e10fdaed8b919
Hengyi Zhao <hzhao94@myseneca.ca>
2023-02-07 00:05:09 -0500

    Back-up Finished

--- server/index.js
+++ server/index.js
@@ -12,8 +12,12 @@ import authRoutes from "./routes/auth.js";
 import userRoutes from "./routes/users.js";
 import postRoutes from "./routes/posts.js";
 import { register } from "./controllers/auth.js";
-import {createPost} from "./controllers/posts.js";
+import { createPost } from "./controllers/posts.js";
 import { verifyToken } from "./middleware/auth.js";
+import User from "./models/User.js";
+import Post from "./models/Post.js";
+import { users, posts } from "./data/index.js";
+
 
 /* CONFIGURATIONS */
 const __filename = fileURLToPath(import.meta.url);
@@ -43,7 +47,7 @@ const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
 app.post("/auto/register", upload.single("picture"), register);
-app.post("/posts", verifyToken, upload.single("picture"),createPost);
+app.post("/posts", verifyToken, upload.single("picture"), createPost);
 
 /* ROUTES */
 app.use("/auth", authRoutes);
@@ -60,6 +64,10 @@ mongoose
   .then(() => {
     app.listen(PORT, () => {
       console.log("Server Port: " + PORT);
+      
+      /* ADD DATA ONE TIME */
+      // User.insertMany(users)
+      // Post.insertMany(posts)
     });
   })
   .catch((err) => {
